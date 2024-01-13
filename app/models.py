5d1a292c6dae7b3b58f479993ce798e83b914493
ac/dc <camisglhdadae@gmail.com>
2024-01-13 20:37:53 +0700

    first model

---
+++ app/models.py
@@ -0,0 +1,72 @@
+# models.py
+
+from unicodedata import category
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Сategory(Base):
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+    charaster_id = Column(Integer, ForeignKey("charaster.id"))
+
+
+class Ally(Base):
+    __tablename__ = "allies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+    charaster_id = Column(Integer, ForeignKey("charaster.id"))
+
+
+class Enemy(Base):
+    __tablename__ = "enemies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+    charaster_id = Column(Integer, ForeignKey("charaster.id"))
+
+
+class Charaster(Base):
+    __tablename__ = "charaster"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    image = Column(String, default="default_images/no_image.png")
+    quote = Column(String, default=None)
+    description = Column(String, default=None)
+    status = Column(String, default="unknown")
+    role = Column(String, default="unknown")
+    kills = Column(Integer, default=0)
+
+    category = relationship("Сategory", backref="charaster")
+    allies = relationship("Ally", backref="charaster")
+    enemies = relationship("Enemy", backref="charaster")
+
+    def __repr__(self):
+        return f"<Charaster(name='{self.name}')>"
+
+
+class Location(Base):
+    __tablename__ = "locations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, default=None)
+
+
+class Weapons(Base):
+    __tablename__ = "weapons"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, default=None)
+
+    image = Column(String, default="default_images/no_image.png")

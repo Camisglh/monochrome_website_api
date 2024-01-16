9d9e81054fdbc6f48f375bf85a6aa563c58b3204
ac/dc <camisglhdadae@gmail.com>
2024-01-16 20:37:20 +0700

    crud location

--- app/crud/location_crud.py
+++ app/crud/location_crud.py
@@ -20,3 +20,27 @@ def create_location(db: Session, location: LocationCreateSchema, image: UploadFi
     db.commit()
     db.refresh(_location)
     return _location
+
+
+def get_location_id(db: Session, location_id: int):
+    return db.query(Location).filter(Location.id == location_id).first()
+
+
+def get_location(db: Session):
+    return db.query(Location).all()
+
+
+def update_location(db: Session, location_id: int, location: LocationCreateSchema):
+    _location = db.query(Location).filter(Location.id == location_id).first()
+    _location.name = location.name
+    _location.description = location.description
+    db.commit()
+    db.refresh(_location)
+    return _location
+
+
+def delete_location(db: Session, location_id: int):
+    _location = db.query(Location).filter(Location.id == location_id).first()
+    db.delete(_location)
+    db.commit()
+    return
--- app/router/location_router.py
+++ app/router/location_router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, UploadFile, File
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas import LocationCreateSchema
-from app.crud.location_crud import create_location
+from app.crud.location_crud import *
 
 location_router = APIRouter()
 
@@ -13,14 +13,32 @@ async def create_location_endpoint(
     db: Session = Depends(get_db),
     image: UploadFile = File(...),
 ):
-    """
-    Create a new location with an optional image.
-
-    :param location: Location data.
-    :param db: Database session.
-    :param image: Optional image file.
-
-    :return: Created location.
-    """
     _location = create_location(db, location, image)
+    return _location
+
+
+@location_router.get("/")
+async def get_locations_endpoint(db: Session = Depends(get_db)):
+    locations = get_location(db)
+    return locations
+
+
+@location_router.get("/{location_id}")
+async def get_location_id_endpoint(location_id: int, db: Session = Depends(get_db)):
+    location = get_location_id(db, location_id)
+    if not location:
+        raise HTTPException(status_code=404, detail="Location not found")
+    return location
+
+
+@location_router.put("/{location_id}")
+async def update_location_id_endpoint(
+    location_id: int,
+    location: LocationCreateSchema = Depends(),
+    db: Session = Depends(get_db),
+    image: UploadFile = File(...),
+):
+    _location = update_location(db, location_id, location)
+    if not _location:
+        raise HTTPException(status_code=404, detail="Location not found")
     return _location
